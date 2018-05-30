a7366c1b8f143979c3e781ae0bd055ce0cd83725
likhitha04 <29768158+likhitha04@users.noreply.github.com>
2018-05-30 17:13:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,8 @@
 from flask import Flask, request, flash, url_for, redirect, render_template
+from models import db,app,userinfo,records,output
 from sqlalchemy import and_
 from sqlalchemy import func
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/speech/sqlalch/database.db'
 
-
-db = SQLAlchemy(app)
-
-class records(db.Model):
-   id = db.Column(db.Integer,primary_key=True)
-   label = db.Column(db.String(50))
-   url = db.Column(db.String(200)) 
-   typeof = db.Column(db.String(20)) 
-   com=db.relationship('output', backref='coms')
-
-   def __repr__(self):
-        return '<records %r>' % self.label
-
-class userinfo(db.Model):
-   id=db.Column(db.Integer,primary_key = True)
-   username = db.Column( db.String(100))
-   password = db.Column(db.String(100))
-   role = db.Column(db.String(100))
-   outputs=db.relationship('output', backref='gives')
-
-   def __repr__(self):
-        return '<userinfo %r>' % self.username
-
-
-class output(db.Model):
-   id = db.Column(db.Integer, primary_key=True)
-   rec_id = db.Column(db.Integer,db.ForeignKey('records.id'))
-   response = db.Column(db.String(50)) 
-   user_id=db.Column(db.Integer, db.ForeignKey('userinfo.id'))
-
-   def __repr__(self):
-        return '<output %r>' % self.response
 
 @app.route('/')
 def hello_world():
@@ -46,40 +11,39 @@ def hello_world():
 @app.route('/result',methods = ['POST', 'GET'])
 def result():
  if request.method == 'POST':
-        if not request.form['inputName'] or not request.form['inputPassword'] :
+        if not request.form['InputName'] or not request.form['InputPassword'] :
          flash('Please enter all the fields' , 'error')
         else:
-         user= userinfo(username=request.form['inputName'], password=request.form['inputPassword'],role=request.form['role'])
+         user= userinfo(username=request.form['InputName'], password=request.form['InputPassword'],role=request.form['InputRl'])
          db.session.add(user)
          db.session.commit()
          return redirect(url_for('show',user=user.id))
  
 @app.route('/show/<user>')
 def show(user):
-    
-    """for rec in recos:
-        var = output.query.filter(and_(output.rec_id == rec.id,output.user_id == user)).first()"""
-    rid=records.query.order_by(func.random()).first().id
-    rec = records.query.get(rid) 
-    var = output.query.filter(and_(output.rec_id == rec.id,output.user_id == user)).first()   
-    if var is None:
-        return render_template('site.html',records = rec,user=user)
+    if((output.query.filter(output.user_id == user).count())!= db.session.query(records).count()):
+       rid=records.query.order_by(func.random()).first().id
+       rec = records.query.get(rid) 
+       var = output.query.filter(and_(output.rec_id == rec.id,output.user_id == user)).first()   
+       if var is None:
+          some=userinfo.query.filter_by(id=user).first()
+          return render_template('site.html',records = rec,user=user,some=some)
+       else:
+          return redirect(url_for('show',user=user))
     else:
-        return redirect(url_for('show',user=user))
-    
-    
+        return 'Completed successfully'
 
-@app.route('/final/<user>',methods = ['POST', 'GET'])
-def final(user):
+@app.route('/final/<user>/<rid>',methods = ['POST', 'GET'])
+def final(user,rid):
  if request.method == 'POST':
-        if not request.form['pid'] or not request.form['ans'] :
+        if not request.form['ans'] :
          flash('Please enter all the fields' , 'error')
         else:
          some=userinfo.query.filter_by(id=user).first()
-         result= output(rec_id=request.form['pid'], response=request.form['ans'],user_id= some.id)
+         result= output(rec_id=rid, response=request.form['ans'],user_id= some.id)
          db.session.add(result)
          db.session.commit()
-         return render_template('output.html',res=output.query.all(),user=user)
+         return redirect(url_for('show',user=user))
        
 if __name__ == '__main__':
    app.run(debug = True)
